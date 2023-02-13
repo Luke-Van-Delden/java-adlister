5f0c712fbc84875fb52b3de45d8728e6b91a6aff
Luke Van Delden <vandelden.luke@gmail.com>
2023-02-13 10:08:26 -0600

    polished

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -24,9 +24,10 @@ public class CreateAdServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         User user = (User) request.getSession().getAttribute("user");
         Ad ad = new Ad(
-                user.getId(), // for now we'll hardcode the user id
-            request.getParameter("title"),
-            request.getParameter("description")
+
+                user.getId(),
+                request.getParameter("title"),
+                request.getParameter("description")
         );
         DaoFactory.getAdsDao().insert(ad);
         response.sendRedirect("/ads");
